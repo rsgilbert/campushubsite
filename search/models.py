ee2a7dd6551c16a3ee47cdf3505eeb5bd078e5a7
rs <g@g.com>
2019-10-16 10:30:49 +0200

    ready for production

--- search/models.py
+++ search/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 class SearchHistory(models.Model):
 	seachword = models.CharField(max_length=255)
+	time = models.DateTimeField(auto_now=True)
+
 
 	def __str__(self):
-		return self.seachword
+		return f'{ self.seachword } on { self.time.date() }'
+
